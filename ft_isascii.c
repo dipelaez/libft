e90d5dddb161c98bf6faa8765202d611d6f70638
dipelaez <diego.pelaez@hotmail.com.br>
2021-07-26 12:06:41 -0300

    update

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,7 +1,5 @@
 int	ft_isascii(int	c)
 {
-	if (c < 0)
-		return (0);
 	if (c >= 0 && c <= 127)
 		return (1);
 	return (0);
--- libft.h
+++ libft.h
@@ -1,4 +1,11 @@
-int ft_isalpha(int  c);
+/**
+ * @brief Checks for an alphabetic character.
+ * 
+ * @return Nonzero if the character c falls into the tested class, and zero if not.
+ * 
+ */
+ int ft_isalpha(int  c);
+
 int	ft_isalnum(int	c);
 int	ft_isascii(int	c);
 int	ft_isdigit(int	c);
