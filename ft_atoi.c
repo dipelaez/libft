38dce810dee0bc500396f7a3c9a0fc5e634aaec1
Diego Pelaez nunes <dipelaez@c1r2p2.42sp.org.br>
2021-08-19 11:44:17 -0300

    header

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dipelaez </var/mail/dipelaez>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/19 11:40:31 by dipelaez          #+#    #+#             */
+/*   Updated: 2021/08/19 11:42:52 by dipelaez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include	"libft.h"
 
 int	ft_atoi(const char *nptr)
 {
-	
-	int neg;
-	int i;
-	int num;
+	int	neg;
+	int	i;
+	int	num;
 
 	i = 0;
 	neg = 1;
 	num = 0;
-	while (nptr[i] == ' ' || nptr[i] == '\n' || nptr[i] == '\t' || nptr[i] == '\v'
-			|| nptr[i] == '\f' || nptr[i] == '\r')
+	while (nptr[i] == ' ' || nptr[i] == '\n' || nptr[i] == '\t'
+		|| nptr[i] == '\v'
+		|| nptr[i] == '\f' || nptr[i] == '\r')
 		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
 	{
