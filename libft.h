34954ebbdc5658334be228210f6e19826fa89477
dipelaez <diego.pelaez@hotmail.com.br>
2021-08-24 16:04:17 -0300

    putchar_fd

--- libft.h
+++ libft.h
@@ -147,4 +147,14 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 */
 void ft_striteri(char *s, void (*f)(unsigned int, char*));
 
+/**
+ * @brief Outputs the character 'c' to the given file
+ * descriptor. 
+ * 
+ * @param c The character to output.
+ * @param fd The file descriptor on which to write. 
+ * @return None.
+*/
+void ft_putchar_fd(char c, int fd);
+
 # endif
